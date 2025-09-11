ec04b4429e18d39136620c07f08b6d9ab05a13dc
KAVEYA-S-21 <113159193+KAVEYA-S-21@users.noreply.github.com>
2025-09-11 23:23:33 +0530

    Update and rename Reference.c to Reference.cpp

---
+++ Reference.cpp
@@ -0,0 +1,21 @@
+#include "ColorCoder.hpp"
+#include <iostream>
+#include <iomanip>
+
+namespace ColorCoder {
+
+    void printReferenceManual() {
+        std::cout << "=== 25-Pair Color Code Reference Manual ===\n";
+        std::cout << "Pair No | Major Color | Minor Color\n";
+        std::cout << "------------------------------------\n";
+
+        for(int pairNum = 1; pairNum <= MAX_MAJOR_COLORS * MAX_MINOR_COLORS; ++pairNum) {
+            ColorPair pair = getColorFromPairNumber(pairNum);
+            std::cout << std::setw(6) << pairNum
+                      << " | " << std::setw(11) << pair.majorColor
+                      << " | " << std::setw(11) << pair.minorColor
+                      << "\n";
+        }
+    }
+
+}
