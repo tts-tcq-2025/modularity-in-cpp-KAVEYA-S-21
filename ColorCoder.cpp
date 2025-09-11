9d5dd657bc05b5d241a5d0eecb31327680b3ecff
KAVEYA-S-21 <113159193+KAVEYA-S-21@users.noreply.github.com>
2025-09-11 23:22:11 +0530

    Update and rename ColorCoder.c to ColorCoder.cpp

---
+++ ColorCoder.cpp
@@ -0,0 +1,26 @@
+#ifndef COLORCODER_HPP
+#define COLORCODER_HPP
+
+#include <string>
+#include <vector>
+
+namespace ColorCoder {
+
+    constexpr int MAX_MAJOR_COLORS = 5;
+    constexpr int MAX_MINOR_COLORS = 5;
+
+    extern const std::vector<std::string> MajorColorNames;
+    extern const std::vector<std::string> MinorColorNames;
+
+    struct ColorPair {
+        std::string majorColor;
+        std::string minorColor;
+    };
+
+    ColorPair getColorFromPairNumber(int pairNumber);
+    int getPairNumberFromColor(const std::string& major, const std::string& minor);
+    void printReferenceManual();
+
+}
+
+#endif
