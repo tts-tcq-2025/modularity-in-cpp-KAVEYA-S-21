d1e025d92b6f1d8af95a73018f93b6ff24ff4573
KAVEYA-S-21 <113159193+KAVEYA-S-21@users.noreply.github.com>
2025-09-11 23:19:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,16 @@
-#include <iostream>
-#include <assert.h>
-#include "ColorCodeUtils.h"
-
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor)
-{
-    auto colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
-
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber)
-{
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-    std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
-}
+#include <stdio.h>
+#include "colorcoder.h"
 
 int main() {
-    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
-    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    // Test translations
+    ColorPair pair = GetColorFromPairNumber(23);
+    printf("Pair 23 is %s - %s\n", pair.majorColor, pair.minorColor);
 
-    std::cout << "\nColor Code Reference Manual:\n";
-    TelCoColorCoder::PrintColorCodeManual();
+    int pairNum = GetPairNumberFromColor("Red", "Green");
+    printf("Red-Green is pair number %d\n", pairNum);
+
+    // Print the reference manual
+    PrintReferenceManual();
 
     return 0;
 }
