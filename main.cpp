57df18a84ccefb598d08c6d09f5c2e611ba7bb3b
KAVEYA-S-21 <113159193+KAVEYA-S-21@users.noreply.github.com>
2025-09-22 11:25:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,9 @@
-#include "ColorCoder.h"
 #include <iostream>
+#include "ColorPair.h"
+#include "ColorUtils.h"
+#include "TestColorPair.h"
 
 int main() {
-    using namespace ColorCoder;
-
-    try {
-        ColorPair pair = getColorFromPairNumber(23);
-        std::cout << "Pair 23 is " << pair.majorColor << " - " << pair.minorColor << "\n";
-
-        int pairNum = getPairNumberFromColor("Red", "Green");
-        std::cout << "Red-Green is pair number " << pairNum << "\n";
-
-        // Print full reference manual
-        printReferenceManual();
-    }
-    catch(const std::exception& ex) {
-        std::cerr << "Error: " << ex.what() << "\n";
-    }
-
+    TelCoColorCoder::RunTests();
     return 0;
-} 
+}
